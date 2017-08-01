c7813210c93dc1d5c076fddd20410be9bd61a585
Sébastien Elet <sebastien@elet.fr>
2017-08-01 14:37:44 +0200

    fix(template): add missing else

--- templates/mongobackup.sh
+++ templates/mongobackup.sh
@@ -3,4 +3,4 @@
 date=$(date +%F)
 backupDir="{{mongo_backup_path}}${date}/"
 mkdir -p "${backupDir}"
-mongodump --db "{{mongo_backup_db}}" {{'--gzip ' if mongo_backup_gzip == true}}--out "${backupDir}"
+mongodump --db "{{mongo_backup_db}}" {{'--gzip ' if mongo_backup_gzip == true else ''}}--out "${backupDir}"
