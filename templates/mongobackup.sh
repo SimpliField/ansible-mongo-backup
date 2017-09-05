bfa5a52a7004c663b4e333a9cd56d0346f9e4934
Sébastien Elet <sebastien@elet.fr>
2017-09-05 12:41:06 +0200

    perf(mongodump): do not use gzip on gridfs

--- templates/mongobackup.sh
+++ templates/mongobackup.sh
@@ -3,4 +3,16 @@
 date=$(date +%F)
 backupDir="{{mongo_backup_path}}${date}/"
 mkdir -p "${backupDir}"
-mongodump --db "{{mongo_backup_db}}" {{'--gzip ' if mongo_backup_gzip == true else ''}}--out "${backupDir}"
+# Dump all except gridfs
+mongodump --db "{{mongo_backup_db}}" \
+  {{'--gzip ' if mongo_backup_gzip == true else ''}}--out "${backupDir}" \
+  --excludeCollection 'fs.files' \
+  --excludeCollection 'fs.chunks'
+# Dump only gridfs and always without gzip
+mongodump --db "{{mongo_backup_db}}" \
+  --out "${backupDir}" \
+  --collection 'fs.files'
+mongodump --db "{{mongo_backup_db}}" \
+  --out "${backupDir}" \
+  --collection 'fs.files' \
+  --collection 'fs.chunks'
